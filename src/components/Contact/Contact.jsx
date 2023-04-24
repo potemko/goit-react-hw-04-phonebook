001be48e379f687433ee9a5986b429e15a6e5ab0
Mykhailo Potemko <105733943+potemko@users.noreply.github.com>
2023-04-24 08:09:35 +0300

    2404

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,61 +1,47 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import { nanoid } from 'nanoid';
 import Phonebook from 'components/Phonebook/Phonebook';
 import Filter from 'components/Filter/Filter';
 
-class Contacts extends Component {
-  state = {
-    filter: '',
-    contacts: [],
-    number: '',
-    name: '',
+function Contact() {
+  const [filter, setFilter] = useState('');
+  const [contacts, setContacts] = useState([]);
+  const [number, setNumber] = useState('');
+  const [name, setName] = useState('');
+
+  const handleChange = event => {
+    event.preventDefault();
+    setName(event.currentTarget.value);
   };
 
-  addContact = event => {
+  const handleNumber = event => {
     event.preventDefault();
-    const { name, contacts } = this.state;
+    setNumber(event.currentTarget.value);
+  };
+  const handleSubmit = event => {
+    event.preventDefault();
 
     if (contacts.some(contact => contact.name === name)) {
       alert(`${name} is already in contacts`);
       return;
     }
-  };
-
-  handleChange = event => {
-    const { name, value } = event.currentTarget;
-    this.setState({ [name]: value });
-  };
-
-  handleSubmit = event => {
-    event.preventDefault();
-
-    const { name, number } = this.state;
-
-    if (this.state.contacts.some(contact => contact.name === name)) {
-      alert(`${name} is already in contacts`);
-      return;
-    }
-
     const newContact = { id: nanoid(), name, number };
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, newContact],
-      name: '',
-      number: '',
-    }));
+    setContacts([...contacts, newContact]);
+    setName('');
+    setNumber('');
   };
 
-  handleDelete = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
+  const handleDelete = contactId => {
+    setContacts(prevState =>
+      prevState.filter(contact => contact.id !== contactId)
+    );
   };
 
-  handleFilterChange = event => {
-    this.setState({ filter: event.currentTarget.value });
+  const handleFilterChange = event => {
+    setFilter(event.currentTarget.value);
   };
 
-  getFilteredContacts = () => {
-    const { contacts, filter } = this.state;
+  const getFilteredContacts = () => {
     const normalizedFilter = filter.toLowerCase();
 
     return contacts.filter(contact =>
@@ -63,28 +49,110 @@ class Contacts extends Component {
     );
   };
 
-  render() {
-    // const { name, number, filter } = this.state;
-    // const filteredContacts = this.getFilteredContacts();
+  return (
+    <div>
+      <Phonebook
+        handleSubmit={handleSubmit}
+        handleChange={handleChange}
+        handleNumber={handleNumber}
+        name={name}
+        number={number}
+      />
 
-    return (
-      <div>
-        <Phonebook
-          upHandleSubmit={this.handleSubmit}
-          uphandleChange={this.handleChange}
-          upName={this.state.name}
-          upNumber={this.state.number}
-        />
-
-        <Filter
-          UpHandleFilterChange={this.handleFilterChange}
-          UphandleDelete={this.handleDelete}
-          UpfilteredContacts={this.getFilteredContacts()}
-          Upfilter={this.state.filter}
-        />
-      </div>
-    );
-  }
+      <Filter
+        UpHandleFilterChange={handleFilterChange}
+        UphandleDelete={handleDelete}
+        UpfilteredContacts={getFilteredContacts()}
+        Upfilter={filter}
+      />
+    </div>
+  );
 }
 
-export default Contacts;
+export default Contact;
+
+// class Contacts extends Component {
+//   state = {
+//     filter: '',
+//     contacts: [],
+//     number: '',
+//     name: '',
+//   };
+
+//   addContact = event => {
+//     event.preventDefault();
+//     const { name, contacts } = this.state;
+
+//     if (contacts.some(contact => contact.name === name)) {
+//       alert(`${name} is already in contacts`);
+//       return;
+//     }
+//   };
+
+//   handleChange = event => {
+//     const { name, value } = event.currentTarget;
+//     this.setState({ [name]: value });
+//   };
+
+//   handleSubmit = event => {
+//     event.preventDefault();
+
+//     const { name, number } = this.state;
+
+//     if (this.state.contacts.some(contact => contact.name === name)) {
+//       alert(`${name} is already in contacts`);
+//       return;
+//     }
+
+//     const newContact = { id: nanoid(), name, number };
+//     this.setState(prevState => ({
+//       contacts: [...prevState.contacts, newContact],
+//       name: '',
+//       number: '',
+//     }));
+//   };
+
+//   handleDelete = contactId => {
+//     this.setState(prevState => ({
+//       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+//     }));
+//   };
+
+//   handleFilterChange = event => {
+//     this.setState({ filter: event.currentTarget.value });
+//   };
+
+//   getFilteredContacts = () => {
+//     const { contacts, filter } = this.state;
+//     const normalizedFilter = filter.toLowerCase();
+
+//     return contacts.filter(contact =>
+//       contact.name.toLowerCase().includes(normalizedFilter)
+//     );
+//   };
+
+//   render() {
+//     // const { name, number, filter } = this.state;
+//     // const filteredContacts = this.getFilteredContacts();
+
+//     return (
+//       <div>
+//         <Phonebook
+//           upHandleSubmit={this.handleSubmit}
+//           uphandleChange={this.handleChange}
+//           upName={this.state.name}
+//           upNumber={this.state.number}
+//         />
+
+//         <Filter
+//           UpHandleFilterChange={this.handleFilterChange}
+//           UphandleDelete={this.handleDelete}
+//           UpfilteredContacts={this.getFilteredContacts()}
+//           Upfilter={this.state.filter}
+//         />
+//       </div>
+//     );
+//   }
+// }
+
+// export default Contacts;
--- src/components/Phonebook/Phonebook.js
+++ src/components/Phonebook/Phonebook.js
@@ -1,16 +1,22 @@
 import React from 'react';
 
-const Phonebook = ({ upHandleSubmit, uphandleChange, upName, upNumber }) => (
+const Phonebook = ({
+  handleSubmit,
+  handleChange,
+  name,
+  number,
+  handleNumber,
+}) => (
   <div>
     <h1>Phonebook</h1>
-    <form onSubmit={upHandleSubmit}>
+    <form onSubmit={handleSubmit}>
       <label>
         Name
         <input
           type="text"
           name="name"
-          value={upName}
-          onChange={uphandleChange}
+          value={name}
+          onChange={handleChange}
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
@@ -21,8 +27,8 @@ const Phonebook = ({ upHandleSubmit, uphandleChange, upName, upNumber }) => (
         <input
           type="tel"
           name="number"
-          value={upNumber}
-          onChange={uphandleChange}
+          value={number}
+          onChange={handleNumber}
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
